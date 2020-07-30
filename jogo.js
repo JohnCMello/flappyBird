07c9db2378e806cd15074f418af3069fedc94077
John Mello <johncharlesmello@gmail.com>
2020-07-29 23:52:14 -0300

    adiciona troca de telas

--- jogo.js
+++ jogo.js
@@ -6,95 +6,155 @@ sprites.src = './assets/sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
+//[StartScreen]
+const startScreen = {
+    spriteX: 134,
+    spriteY: 0,
+    width: 174,
+    height: 152,
+    canvasX: ( canvas.width / 2 ) - 174 / 2,
+    canvasY: 50,
+    print(){
+        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.width, sHeight.height, dx.vancasX, dy.canvasY, dWidth.width, dHeight.height);
+            sprites,
+            startScreen.spriteX, startScreen.spriteY, //inicio do recorte X>,Y^;
+            startScreen.width, startScreen.height, //width, height recorte na sprite; 
+            startScreen.canvasX, startScreen.canvasY, // onde vai ser printdo no canvas;
+            startScreen.width, startScreen.height, //width, height, dentro do canvas;
+        );   
+    }
+}
+
 //[Background]
 const background = {
     spriteX: 390,
     spriteY: 0,
-    largura: 275,
-    altura: 204,
+    width: 275,
+    height: 204,
     canvasX: 0,
     canvasY: canvas.height - 204,
-    desenha(){
+    print(){
         contexto.fillStyle = '#70c6ce';
         contexto.fillRect(0,0,canvas.width,canvas.height); //ctx.fillRect(x, y, width, height);
         
-        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.largura, sHeight.altura, dx.vancasX, dy.canvasY, dWidth.largura, dHeight.altura);
-            sprites,
-            background.spriteX, background.spriteY, //inicio do recorte X>,Y^;
-            background.largura, background.altura, //largura, altura recorte na sprite;
-            background.canvasX, background.canvasY, // onde vai ser desenhado no canvas;
-            background.largura, background.altura, //largura, altura, dentro do canvas;
+        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.width, sHeight.height, dx.vancasX, dy.canvasY, dWidth.width, dHeight.height);
+        sprites,
+        background.spriteX, background.spriteY, //inicio do recorte X>,Y^;
+        background.width, background.height, //width, height recorte na sprite;
+        background.canvasX, background.canvasY, // onde vai ser printdo no canvas;
+        background.width, background.height, //width, height, dentro do canvas;
         );
         contexto.drawImage(
             sprites,
             background.spriteX, background.spriteY, //inicio do recorte X>,Y^;
-            background.largura, background.altura, //largura, altura recorte na sprite; 
-            (background.canvasX + background.largura) , background.canvasY, // onde vai ser desenhado no canvas;
-            background.largura, background.altura, //largura, altura, dentro do canvas;
+            background.width, background.height, //width, height recorte na sprite; 
+            (background.canvasX + background.width) , background.canvasY, // onde vai ser printdo no canvas;
+            background.width, background.height, //width, height, dentro do canvas;
         );
     }
 }
+    
 //[Player]
 const flappyBird = {
     spriteX: 0,
     spriteY: 0,
-    largura: 33,
-    altura: 24,
+    width: 33,
+    height: 24,
     canvasX: 10,
     canvasY: 50,
     gravidade: 0.25,
     velocidade: 0,
-    atualiza(){
+    reload(){
         flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
         console.log(flappyBird.velocidade);
         flappyBird.canvasY = flappyBird.canvasY + flappyBird.velocidade;        
     },
-    desenha(){
-        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.largura, sHeight.altura, dx.vancasX, dy.canvasY, dWidth.largura, dHeight.altura);
-        sprites,
-        flappyBird.spriteX, flappyBird.spriteY, //inicio do recorte X>,Y^;
-        flappyBird.largura, flappyBird.altura, //largura, altura recorte na sprite; 
-        flappyBird.canvasX, flappyBird.canvasY, // onde vai ser desenhado no canvas;
-        flappyBird.largura, flappyBird.altura, //largura, altura, dentro do canvas;
+    print(){
+        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.width, sHeight.height, dx.vancasX, dy.canvasY, dWidth.width, dHeight.height);
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, //inicio do recorte X>,Y^;
+            flappyBird.width, flappyBird.height, //width, height recorte na sprite; 
+            flappyBird.canvasX, flappyBird.canvasY, // onde vai ser printdo no canvas;
+            flappyBird.width, flappyBird.height, //width, height, dentro do canvas;
         );
     }
 }
+
+
 //[Floor]
 const floor = {
     spriteX: 0,
     spriteY: 610,
-    largura: 224,
-    altura: 112,
+    width: 224,
+    height: 112,
     canvasX: 0,
     canvasY: canvas.height - 112,
-    desenha(){
-        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.largura, sHeight.altura, dx.vancasX, dy.canvasY, dWidth.largura, dHeight.altura);
-        sprites,
-        floor.spriteX, floor.spriteY, //inicio do recorte X>,Y^;
-        floor.largura, floor.altura, //largura, altura recorte na sprite; 
-        floor.canvasX, floor.canvasY, // onde vai ser desenhado no canvas;
-        floor.largura, floor.altura, //largura, altura, dentro do canvas;
-        );
-        contexto.drawImage(
+    print(){
+        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.width, sHeight.height, dx.vancasX, dy.canvasY, dWidth.width, dHeight.height);
             sprites,
             floor.spriteX, floor.spriteY, //inicio do recorte X>,Y^;
-            floor.largura, floor.altura, //largura, altura recorte na sprite; 
-            (floor.canvasX + floor.largura), floor.canvasY, // onde vai ser desenhado no canvas;
-            floor.largura, floor.altura, //largura, altura, dentro do canvas;
-            );
-        }
-        
+            floor.width, floor.height, //width, height recorte na sprite; 
+            floor.canvasX, floor.canvasY, // onde vai ser printdo no canvas;
+            floor.width, floor.height, //width, height, dentro do canvas;
+        );
+        contexto.drawImage(//(image(sprites), sx.spriteX, sy.spriteY, sWidth.width, sHeight.height, dx.vancasX, dy.canvasY, dWidth.width, dHeight.height);
+            sprites,
+            floor.spriteX, floor.spriteY, //inicio do recorte X>,Y^;
+            floor.width, floor.height, //width, height recorte na sprite; 
+            (floor.canvasX + floor.width), floor.canvasY, // onde vai ser printdo no canvas;
+            floor.width, floor.height, //width, height, dentro do canvas;
+        );
     }
+}
+
+//[Screens]
+let activeScreen = {};
+
+function changeScreen(newScreen){
+    activeScreen = newScreen;
+}
+
+const screens = {
+    START:{
+        print(){
+        background.print();
+        floor.print();
+        flappyBird.print();
+        startScreen.print();
+        },
+        click(){
+            changeScreen(screens.GAME)
+        },
+        reload(){
+
+        }
+    }
+};
+
+screens.GAME = {
+    print(){
+        background.print();
+        floor.print();
+        flappyBird.print();
+    },
+    reload(){
+        flappyBird.reload();
+
+    }
+};
     
-    
-    function loop(){
-        flappyBird.atualiza();
-        
-        background.desenha();
-        flappyBird.desenha();
-        floor.desenha();
-        
+function loop(){
+    activeScreen.print();
+    activeScreen.reload();
     requestAnimationFrame(loop);
 }
 
+window.addEventListener('click', function(){
+    if(activeScreen.click){
+        activeScreen.click();
+    }
+
+});
+
+changeScreen(screens.START)   
 loop();
